a882c35e14bca43e753b9f3082404cf49f9d55d9
SakthiKrish155 <125114424+SakthiKrish155@users.noreply.github.com>
2024-08-07 12:32:22 +0530

    Add files via upload

---
+++ Backend/src/main/java/com/sdp/taskandtimemanager/model/Users.java
@@ -0,0 +1,209 @@
+package com.sdp.taskandtimemanager.model;
+
+// import java.time.LocalDate;
+
+// import java.util.List;
+
+// import com.fasterxml.jackson.annotation.JsonIgnore;
+
+// import jakarta.persistence.CascadeType;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.FetchType;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.OneToMany;
+
+// @Entity
+// public class Users {
+
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long userid;
+//     private String username;
+//     private String password;
+//     private String email;
+//     private String role;
+//     private LocalDate dob;
+//     private String contact;
+
+//     @JsonIgnore
+//     @OneToMany(mappedBy = "manager", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+//     private List<Projects> project;
+
+//     @JsonIgnore
+//     @OneToMany(mappedBy = "member", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+//     private List<Tasks> tasks;
+
+//     public Long getUserid() {
+//         return userid;
+//     }
+
+//     public void setUserid(Long userid) {
+//         this.userid = userid;
+//     }
+
+//     public String getUsername() {
+//         return username;
+//     }
+
+//     public void setUsername(String username) {
+//         this.username = username;
+//     }
+
+//     public String getPassword() {
+//         return password;
+//     }
+
+//     public void setPassword(String password) {
+//         this.password = password;
+//     }
+
+//     public String getEmail() {
+//         return email;
+//     }
+
+//     public void setEmail(String email) {
+//         this.email = email;
+//     }
+
+//     public String getRole() {
+//         return role;
+//     }
+
+//     public void setRole(String role) {
+//         this.role = role;
+//     }
+
+//     public LocalDate getDob() {
+//         return dob;
+//     }
+
+//     public void setDob(LocalDate dob) {
+//         this.dob = dob;
+//     }
+
+//     public String getContact() {
+//         return contact;
+//     }
+
+//     public void setContact(String contact) {
+//         this.contact = contact;
+//     }
+
+//     public List<Projects> getProject() {
+//         return project;
+//     }
+
+//     public void setProject(List<Projects> project) {
+//         this.project = project;
+//     }
+
+//     public List<Tasks> getTasks() {
+//         return tasks;
+//     }
+
+//     public void setTasks(List<Tasks> tasks) {
+//         this.tasks = tasks;
+//     }
+
+// }
+
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+// import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Users implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userid;
+    private String name;
+    private String password;
+    private String email;
+    private LocalDate dob;
+    private String contact;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public enum Role {
+        USER, ADMIN, PROJECTMANAGER
+    }
+    
+    @JsonIgnore
+    @OneToMany(mappedBy = "manager", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Projects> project;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Tasks> tasks;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user")
+    private List<Token> tokens;
+
+    @Override
+    @JsonIgnore
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+    
+    @Override
+    @JsonIgnore
+    public String getUsername() {
+        // NOTE : return username, if you are using username for login instead of email
+        return email;
+    }
+    
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    
+    @Override
+    @JsonIgnore
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    
+    @Override
+    @JsonIgnore
+    public boolean isEnabled() {
+        return true;
+    }
+}
